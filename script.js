caf367485701c485b47650db2b2cc823bb910dc2
Drishti Sharma <20drishti06@gmail.com>
2026-02-07 12:34:51 +0530

    e

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const screens = document.querySelectorAll(".screen");
 function nextScreen() {
   screens[current].classList.remove("active");
   current++;
+
   if (current < screens.length) {
     screens[current].classList.add("active");
   }
