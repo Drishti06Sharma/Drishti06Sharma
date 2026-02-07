daf73c1ba53e4c9ab6065dba01cd52b49b75431a
Drishti Sharma <20drishti06@gmail.com>
2026-02-07 12:54:16 +0530

    n

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const lines = document.querySelectorAll(".shayari p");
 
-lines.forEach((line, index) => {
-  setTimeout(() => {
-    line.style.opacity = 1;
-    line.style.transition = "opacity 1.5s ease";
-  }, index * 1400);
-});
